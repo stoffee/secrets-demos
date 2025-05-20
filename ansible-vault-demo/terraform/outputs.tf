f95e3a7a3f2cb8d7a87c234e67146c15ef73d666
stoffee <cdunlap@hashicorp.com>
2025-05-20 10:59:02 -1000

    added auto-rotation

--- ansible-vault-demo/terraform/outputs.tf
+++ ansible-vault-demo/terraform/outputs.tf
@@ -7,8 +7,8 @@ output "vault_public_url" {
   value = hcp_vault_cluster.hcp_vault.vault_public_endpoint_url
 }
 
-output "app_server_public_ip" {
-  value = aws_instance.app_server.public_ip
+output "app_server_url" {
+  value = "http://${aws_instance.app_server.public_ip}:5000/"
 }
 
 output "ansible_role_id" {
